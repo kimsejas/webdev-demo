dfc6cddfa3a896c9a2fc1e861f72c660899de9b8
kimsejas <sejaskm@gmail.com>
2024-11-12 12:28:37 -0500

    Done

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.getElementById("submitButton").addEventListener("click", addTask);
+function addTask(){
+    const taskInputVal = document.getElementById("taskInput").value;
+    if (taskInputVal){
+        const newTask = document.createElement('li');
+        newTask.innerText= taskInputVal;
+        document.getElementById("taskList").appendChild(newTask)
+    
+        const deleteButton = document.createElement("button");
+        deleteButton.className="button";
+        deleteButton.innerText="x";
+        newTask.appendChild(deleteButton)
+        deleteButton.addEventListener("click", () => taskList.removeChild(newTask));
+
+        document.getElementById('taskInput').value=''
+    }
+}
 
